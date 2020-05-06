023768649bb8127bcce4b3fae5f16bcbc21b02bf
Araya Nawrath <arayanawrath@gmail.com>
2020-05-05 23:27:00 -0400

    change

--- config/orm.js
+++ config/orm.js
@@ -65,9 +65,9 @@ var orm = {
         second = '0' + second;
       }
     timestamp += second;
-    // ----------------------------------------------------------
+    
 
-    // Run MySQL Query
+    
     connection.query('INSERT INTO burgers SET ?', {
       burger_name: burger_name,
       devoured: false,
@@ -79,10 +79,10 @@ var orm = {
 
   },
 
-  // updateOne()
+  
   updateOne: function(burgerID, callback){
 
-    // Run MySQL Query
+  
     connection.query('UPDATE burgers SET ? WHERE ?', [{devoured: true}, {id: burgerID}], function (err, result) {
         if (err) throw err;
         callback(result);
