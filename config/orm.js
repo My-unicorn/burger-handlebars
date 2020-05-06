a4e7428305aa4d426c7b954486bf0ee731da1ac7
Araya Nawrath <arayanawrath@gmail.com>
2020-05-05 23:23:19 -0400

    orm

--- config/orm.js
+++ config/orm.js
@@ -3,7 +3,7 @@ var connection = require('./connection.js');
 
 
 
-// Connect to MySQL database
+
 connection.connect(function(err) {
   if (err) {
     console.error('error connecting: ' + err.stack);
@@ -15,13 +15,12 @@ connection.connect(function(err) {
 
 
 
-// Methods for MySQL commands
 var orm = {
 
-  // selectAll()
+
   selectAll: function(callback) {
 
-    // Run MySQL Query
+   
     connection.query('SELECT * FROM burgers', function (err, result) {
       if (err) throw err;
       callback(result);
@@ -29,20 +28,19 @@ var orm = {
 
   },
 
-  // insertOne()
+  
   insertOne: function(burger_name, callback){
 
-    // Create a new timestamp
-    // ----------------------------------------------------------
+
     var d = new Date();
-    var timestamp = ''+ d.getFullYear() + '-'; // must be string
-    var month = '' + (d.getMonth() + 1); // must be string
-      // handle 1 digit months
+    var timestamp = ''+ d.getFullYear() + '-'; 
+    var month = '' + (d.getMonth() + 1); 
+    
       if(month.length == 1){
         month = '0' + month;
       }
     timestamp += month + '-';
-    var day = '' + d.getDate(); // must be string
+    var day = '' + d.getDate(); 
       // handle 1 digit day of month
       if(day.length == 1){
         day = '0' + day;
