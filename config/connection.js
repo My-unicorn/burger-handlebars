2b4b97a3bfeb51ba5c6d8fd98e72e3ac700a0aed
Araya Nawrath <arayanawrath@gmail.com>
2020-06-06 00:31:54 -0400

    add again to heroku

--- config/connection.js
+++ config/connection.js
@@ -7,10 +7,10 @@ if(process.env.JAWSDB_URL){
 }
 else{
   connection = mysql.createConnection({
-    host     : 'localhost',
-    user     : 'root',
-    password : 'Ariella3', 
-    database : 'burgers_db' 
+    host     : 'qf5dic2wzyjf1x5x.cbetxkdyhwsb.us-east-1.rds.amazonaws.com',
+    user     : 'l9xb0lebcu01swyj',
+    password : 'dx2xi7wyzvodt1pk', 
+    database : 'fhzvhxksgzj5pszs' 
   });
 }
 
